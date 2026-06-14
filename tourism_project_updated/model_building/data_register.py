38910a4e7f8d7bf766528bfa1962f4d59c5c8253
SatishPadmanabhan <satishp0879@gmail.com>
2026-06-14 11:44:08 +0000

    first commit

--- tourism_project_updated/model_building/data_register.py
+++ tourism_project_updated/model_building/data_register.py
@@ -12,14 +12,14 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 # Step 1: Check if the space exists
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Space '{repo_id}' already exists. Using it.")
+    print(f"Dataset repository '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
-    print(f"Space '{repo_id}' not found. Creating new space...")
+    print(f"Dataset repository '{repo_id}' not found. Creating new one...")
     create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
-    print(f"Space '{repo_id}' created.")
+    print(f"Dataset repository '{repo_id}' created.")
 
 api.upload_folder(
-    folder_path="tourism_project/data",
+    folder_path="tourism_project_updated/data",
     repo_id=repo_id,
     repo_type=repo_type,
 )
